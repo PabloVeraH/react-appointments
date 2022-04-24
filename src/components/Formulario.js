a2bfa25ddf083f2cfa94aa577295b8e99eac23fc
Pablo Vera <pablo.vera82@gmail.com>
2022-04-24 17:59:44 -0400

    Advance with the appointment form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,17 +1,52 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 
 const Formulario = () => {
+    
+    // Crear State de Citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    const actualizarState = (e) => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    // Extraer los valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+    
+    // Cuando el usuario presiona agregar cita
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+
+        // Asignar un ID
+
+        // Crear la cita
+
+        // Reiniciar el form
+    }
+
     return (
         <Fragment>
             <h2>Crear Cita</h2>
 
-            <form>
+            <form onSubmit={submitCita} >
                 <label>Nombre Mascota</label>
                 <input 
                     type="text"
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre Mascota"
+                    onChange={actualizarState}
+                    value={mascota}
                 />
 
                 <label>Nombre Dueño</label>
@@ -20,6 +55,8 @@ const Formulario = () => {
                     name="propietario"
                     className="u-full-width"
                     placeholder="Nombre  Dueño de la mascota"
+                    onChange={actualizarState}
+                    value={propietario}
                 />
 
                 <label>Fecha</label>
@@ -27,6 +64,7 @@ const Formulario = () => {
                     type="date"
                     name="fecha"
                     className="u-full-width"
+                    onChange={actualizarState}
                     value={fecha}
                 />
 
@@ -35,6 +73,7 @@ const Formulario = () => {
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={actualizarState}
                     value={hora}
                 />
 
@@ -42,6 +81,8 @@ const Formulario = () => {
                 <textarea
                     className="u-full-width"
                     name="sintomas"
+                    onChange={actualizarState}
+                    value={sintomas}
                 ></textarea>
 
                 <button
