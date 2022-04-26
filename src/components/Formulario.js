a4d42ab320a40896db78228c115d29b00b83f909
Pablo Vera <pablo.vera82@gmail.com>
2022-04-25 21:58:17 -0400

    Add appointment state to app

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const Formulario = () => {
+const Formulario = ({crearCita}) => {
 
     // Crear State de Citas
     const [cita, actualizarCita] = useState({
@@ -40,7 +40,7 @@ const Formulario = () => {
         cita.id = uuidv4();
 
         // Crear la cita
-        //crearCita(cita);
+        crearCita(cita);
 
         // Reiniciar el form
     }
